61552d4dda88f50ce098af1226e657672d31d15a
Marco Bettiolo <marco.bettiolo@7digital.com>
2015-02-27 11:34:04 +0000

    support for vertical screen disposition

--- show-dashboard.sh
+++ show-dashboard.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
+DISPOSITION="horizontal"
+while test $# -gt 0
+do
+    case $1 in
+        --vertical)
+            DISPOSITION="vertical"
+            shift
+            ;;
+    esac
+    shift
+done
+
 if [ ! -f ~/.dashy ]; then
   echo "Initialising ~/.dashy config..."
   echo "$(uuidgen)" > ~/.dashy
 else
   echo "Using existing ~/.dashy config."
 fi
+
 GIT_VER="ver=$(git rev-parse HEAD)"
 
 DASHBOARD1_ID="$(sed -n 1p ~/.dashy)"
@@ -20,8 +33,12 @@ if [ -n "$DASHBOARD2_ID" ]; then
 fi
 
 PRIMARY_DISPLAY_WIDTH="$(xrandr | grep "*" | xargs | cut -d " " -f 1 | cut -d "x" -f 1)"
+PRIMARY_DISPLAY_HEIGHT="$(xrandr | grep "*" | xargs | cut -d " " -f 1 | cut -d "x" -f 2)"
 echo "Primary display width: ${PRIMARY_DISPLAY_WIDTH}"
-
+echo "Primary display height: ${PRIMARY_DISPLAY_HEIGHT}"
+if [ -n "$DASHBOARD2_ID" ]; then
+  echo "Dashboard multi-screen disposition: ${DISPOSITION}"
+fi
 
 printf "Waiting for api.dashy.io to be available: "
 until $(curl --output /dev/null --silent --head --fail http://api.dashy.io/status); do
@@ -31,11 +48,16 @@ done
 printf "OK\r\n"
 
 if command -v google-chrome-stable >/dev/null 2>&1; then
-  echo "Running dashboard on primary screen with google-chrome-stable"
+  echo "Running google-chrome-stable on primary screen"
   google-chrome-stable --incognito --no-first-run --start-fullscreen --window-position=0,0 --user-data-dir="$(mktemp -d)" ${DASHBOARD1_URL} &
   if [ -n "$DASHBOARD2_ID" ]; then
-    echo "Running dashboard on secondary screen with google-chrome-stable"
-    google-chrome-stable --incognito --no-first-run --start-fullscreen --window-position=${PRIMARY_DISPLAY_WIDTH},0 --user-data-dir="$(mktemp -d)" ${DASHBOARD2_URL} &
+    if [ "$DISPOSITION" == "horizontal" ]; then
+      echo "Running google-chrome-stable on secondary screen (horizontal layout)"
+      google-chrome-stable --incognito --no-first-run --start-fullscreen --window-position=${PRIMARY_DISPLAY_WIDTH},0 --user-data-dir="$(mktemp -d)" ${DASHBOARD2_URL} &
+    else
+      echo "Running google-chrome-stable on secondary screen (vertical layout)"
+      google-chrome-stable --incognito --no-first-run --start-fullscreen --window-position=$0,{PRIMARY_DISPLAY_HEIGHT} --user-data-dir="$(mktemp -d)" ${DASHBOARD2_URL} &
+    fi
   fi
   exit 0
 fi

376d34216365d90abfcd30ada8257e4436d667ea
Marco Bettiolo <marco.bettiolo@7digital.com>
2015-03-02 18:40:37 +0000

    Improving raspbian install

--- raspbian/install.sh
+++ raspbian/install.sh
@@ -4,8 +4,8 @@ echo "Installing on Raspberry Pi..."
 
 echo "Updating packages"
 sudo apt-get update
-sudo apt-get upgrade
-sudo apt-get dist-upgrade
+sudo apt-get --yes upgrade
+sudo apt-get --yes dist-upgrade
 
 echo "Updating firmware"
 sudo rpi-update
@@ -15,25 +15,21 @@ if [ ! -f /etc/lightdm/lightdm.conf.ori ]; then
 	sudo mv /etc/lightdm/lightdm.conf /etc/lightdm/lightdm.conf.ori
 fi
 
-if [ ! -f /etc/lightdm/lightdm.conf ]; then
-	echo "Configuring lightdm to autologin and disable screen blanking"
-	sudo cp lightdm.conf /etc/lightdm/lightdm.conf
-fi
+echo "Configuring lightdm to autologin and disable screen blanking"
+sudo cp lightdm.conf /etc/lightdm/lightdm.conf
 
 if [ ! -f /etc/xdg/lxsession/LXDE/autostart.ori ]; then
 	echo "Backing up original LXDE autostart config to /etc/xdg/lxsession/LXDE/autostart.ori"
 	sudo mv /etc/xdg/lxsession/LXDE/autostart /etc/xdg/lxsession/LXDE/autostart.ori
 fi
 
-if [ ! -f /etc/xdg/lxsession/LXDE/autostart ]; then
-	echo "Configuring LXDE to hide the mouse cursor, disable screen blanking and start the browser..."
-	sudo cp autostart /etc/xdg/lxsession/LXDE/autostart
-fi
+echo "Configuring LXDE to hide the mouse cursor, disable screen blanking and start the browser..."
+sudo cp autostart /etc/xdg/lxsession/LXDE/autostart
 
 echo "Installing Dashy dependency: unclutter"
-sudo apt-get install unclutter
+sudo apt-get --yes install unclutter
 
 echo "Installing Dashy dependency: chromium"
-sudo apt-get install chromium
+sudo apt-get --yes install chromium
 
 echo "Done."

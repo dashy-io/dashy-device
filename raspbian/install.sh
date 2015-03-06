4bc71c42802b2731847b3e77cb15663be5b1544b
Marco Bettiolo <marco.bettiolo@7digital.com>
2015-03-06 10:58:05 +0000

    Improved install messages

--- raspbian/install.sh
+++ raspbian/install.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "Installing on Raspberry Pi..."
+echo "Installing Dashy on Raspberry Pi..."
 
 read -p "Do you want to update the system? [Y/n] " -n 1 -r
 echo
 if [[ ! $REPLY =~ ^[Nn]$ ]]
 then
-	echo "Updating packages"
+	echo "Updating packages..."
 	sudo apt-get update
 	sudo apt-get --yes upgrade
 	sudo apt-get --yes dist-upgrade
-	echo "Updating firmware"
+	echo "Updating firmware..."
 	sudo rpi-update
 fi
 
@@ -27,7 +27,7 @@ if [ ! -f /etc/xdg/lxsession/LXDE-pi/autostart.ori ]; then
 	sudo mv /etc/xdg/lxsession/LXDE-pi/autostart /etc/xdg/lxsession/LXDE-pi/autostart.ori
 fi
 
-echo "Configuring LXDE-pi to hide the mouse cursor, disable screen blanking and start the browser..."
+echo "Configuring LXDE-pi to hide the mouse cursor, disable screen blanking and start the browser"
 sudo cp autostart /etc/xdg/lxsession/LXDE-pi/autostart
 
 if [ ! -f /etc/crontab.ori ]; then
